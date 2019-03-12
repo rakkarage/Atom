43df934ca05ed9ab1998306afc813002c21dd138
Rakka Rage <rakkarage@gmail.com>
2019-03-12 10:33:18 -0400

    x

--- Assets/Atom/AtomReference.cs
+++ Assets/Atom/AtomReference.cs
@@ -3,7 +3,16 @@ namespace ca.HenrySoftware.Atom
 {
 	public abstract class AtomReference<T> : ScriptableObject
 	{
-		public T Value;
-		// todo: need events!
+		public AtomEvent<T> Changed;
+		private T _value;
+		public T Value
+		{
+			get { return _value; }
+			set
+			{
+				_value = value;
+				Changed.Raise(_value);
+			}
+		}
 	}
 }
--- Assets/Test/TestPlayer.cs
+++ Assets/Test/TestPlayer.cs
@@ -13,8 +13,7 @@ public class TestPlayer : MonoBehaviour
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		TestMob mob = other.gameObject.GetComponent<TestMob>();
-		Debug.Log(mob.name);
-		// if (mob != null)
-		// 	Health -= mob.Damage;
+		if (mob != null)
+			Health.Value -= mob.Damage.Value;
 	}
 }
