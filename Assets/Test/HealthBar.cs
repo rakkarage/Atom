f7456512205cd128f431f293ca84650d27c52768
Rakka Rage <rakkarage@gmail.com>
2019-03-24 13:17:44 -0400

    respond to event from canvas image

---
+++ Assets/Test/HealthBar.cs
@@ -0,0 +1,21 @@
+﻿using ca.HenrySoftware.Atom;
+using UnityEngine;
+using UnityEngine.UI;
+[RequireComponent(typeof(Image))]
+public class HealthBar : MonoBehaviour
+{
+	public AtomInt Current;
+	public AtomInt Max;
+	public Gradient Colors;
+	private Image _image;
+	private void Awake()
+	{
+		_image = GetComponent<Image>();
+	}
+	public void UpdateCurrent(int current)
+	{
+		var percent = (float)Current.Value / (float)Max.Value;
+		_image.color = Colors.Evaluate(percent);
+		_image.fillAmount = percent;
+	}
+}
